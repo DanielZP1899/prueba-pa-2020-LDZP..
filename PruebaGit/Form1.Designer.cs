87cc686adbac6cfb55aed916624c7c58d504870e
DanielZP1899 <denny_1809@hotmail.com>
2020-05-04 18:49:37 -0500

    Se agrega etiqueta

--- PruebaGit/Form1.Designer.cs
+++ PruebaGit/Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label1.ForeColor = System.Drawing.Color.Lime;
-            this.label1.Location = new System.Drawing.Point(156, 51);
+            this.label1.Location = new System.Drawing.Point(149, 90);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(213, 24);
             this.label1.TabIndex = 1;
